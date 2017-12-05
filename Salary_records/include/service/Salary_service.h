78611e186d3dd4a1b3d82aee1a5c041289bb4084
arudkov14 <andrey14@ru.is>
2017-12-05 14:09:25 +0000

    new units

--- Salary_records/include/service/Salary_service.h
+++ Salary_records/include/service/Salary_service.h
@@ -3,6 +3,8 @@
 #include "Salary_record.h"
 #include <iostream>
 #include "Salary_repo.h"
+#include "InvalidMonthException.h"
+#include "InvalidYearException.h"
 using namespace std;
 
 
@@ -10,6 +12,7 @@ class Salary_service
 {
     public:
         void add_salary(const Salary_record& salary);
+        bool is_valid_salary( Salary_record salary);
 
     private:
         Salary_repo salary_repo;
--- Salary_records/src/service/Salary_service.cpp
+++ Salary_records/src/service/Salary_service.cpp
@@ -3,5 +3,35 @@
 
 void Salary_service::add_salary(const Salary_record& salary)
 {
-    salary_repo.add_salary(salary);
+    if(is_valid_salary(salary))
+    {
+        salary_repo.add_salary(salary);
+    }
+}
+
+bool Salary_service::is_valid_salary(Salary_record salary)
+{
+    try
+    {
+        if(salary.get_month() > 12 || salary.get_month() < 1)
+        {
+            throw InvalidMonthException();
+        }
+        else if(salary.get_year() != 2017)
+        {
+            throw InvalidYearException();
+        }
+    }
+    catch(InvalidMonthException e)
+        {
+            cout << "There is only 12 months";
+            return false;
+        }
+    catch(InvalidYearException e)
+    {
+        cout << "Only 2017";
+        return false;
+    }
+
+    return true;
 }
